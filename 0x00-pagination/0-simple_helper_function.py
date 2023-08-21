6673faf3299efabff176a6114b68c9878632a78e
Daniel John <dj71286@gmail.com>
2023-08-21 15:23:56 +0100

    Fulfilled code check requirements

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-""" Pagination """
+#!/usr/bin/env python3
+""" Pagination Algo in Python """
 
 
 def index_range(page, page_size):
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" The Server Class """
 import csv
 import math
 from typing import List
